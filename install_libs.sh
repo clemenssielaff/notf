6982a0ce574b0e9f93484b3f7f74c9157b1c3fdc
Clemens Sielaff <clemenssielaff@users.noreply.github.com>
2016-10-26 09:00:50 +1300

    Updated install_libs.sh to include jekyll

--- install_libs.sh
+++ install_libs.sh
@@ -8,3 +8,7 @@ sudo apt-get -y install freeglut3-dev
 sudo apt-get -y install libxrandr-dev
 sudo apt-get -y install libxcursor-dev
 sudo apt-get -y install libxinerama-dev
+
+# github-pages
+sudo apt-get install ruby2.3-dev
+sudo gem install jekyll
