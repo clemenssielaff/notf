e3587780ad9fe9214e7925306d22341bf71cb501
Clemens Sielaff <git@clemens-sielaff.com>
2018-12-10 09:45:14 +1300

    Last update on Linux before switching to Windows for testing

--- tests/src/app/test_timer_pool.cpp
+++ tests/src/app/test_timer_pool.cpp
@@ -1,46 +1,14 @@
 #include "catch2/catch.hpp"
 
-#include <iostream>
-
-#include "test_app_utils.hpp"
-#include "test_utils.hpp"
-
-#include "notf/common/thread.hpp"
+#include "test/app.hpp"
 
 #include "notf/app/application.hpp"
-#include "notf/app/window.hpp"
-
-SCENARIO("TheApplication Singleton", "[app][application]") {
-
-    SECTION("no instance") {
-        SECTION("Requesting TheApplication without a running instance will throw `SingletonError`") {
-            REQUIRE_THROWS_AS(TheApplication()->get_arguments(), SingletonError);
-        }
-    }
-
-    SECTION("valid instance") {
-        TheApplication app(TheApplication::Arguments{});
-
-        SECTION("can be accessed anywhere in the code") { REQUIRE(TheApplication()->get_arguments().argc == -1); }
-
-        SECTION("can be started (with exec) and shut down") {
-            int result = TheApplication()->exec(); // does not block since no Windows are open
-            TheApplication()->shutdown();
-            REQUIRE(result == EXIT_SUCCESS);
-        }
-
-        SECTION("can not run exec more than  once") {
-            TheApplication()->exec();
-            REQUIRE_THROWS_AS(TheApplication()->exec(), TheApplication::StartupError);
-        }
-
-        SECTION("you can test whether this is the UI thread from anywhere") {
-            REQUIRE(this_thread::is_the_ui_thread());
-            bool hopefully_not = true;
-            Thread().run([&] { hopefully_not = this_thread::is_the_ui_thread(); });
-            REQUIRE(!hopefully_not);
-        }
-    }
-}
+#include "notf/app/timer_pool.hpp"
 
 NOTF_USING_NAMESPACE;
+
+SCENARIO("TimerPool", "[app][timer_pool]") {
+    SECTION("without an application") { REQUIRE_THROWS_AS(*TheTimerPool(), SingletonError); }
+
+    SECTION("with a properly initialized application") { TheApplication app(TheApplication::Arguments{}); }
+}
